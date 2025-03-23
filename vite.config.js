38bbfe79d9a9cb58bb4832ece659b332afc414d4
Jishnu Suresh <jishnusuresh307@gmail.com>
2025-03-23 21:48:09 +0530

    something

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Allows access from external devices
+    port: 5173, // Ensures correct port is used
+    strictPort: true, // Prevents Vite from changing the port
+    allowedHosts: ['.loca.lt'], // Allows LocalTunnel access
+  },
 })
